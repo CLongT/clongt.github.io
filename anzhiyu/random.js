a750a7136558d99609e00e6439c981434510171f
long <3028436309@qq.com>
2025-03-18 10:52:15 +0800

    Site updated: 2025-03-18 10:52:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/13/hexo/butterfly主题配置/","2025/03/12/hexo/hello-world/","2025/03/13/hexo/如何用Hexo搭建个人博客/","2025/03/16/前端开发/vue入门/","2025/03/17/前端开发/在 Vite 项目中配置 @ 路径提示/"];function toRandomPost(){
+var posts=["2025/03/13/hexo/butterfly主题配置/","2025/03/12/hexo/hello-world/","2025/03/17/前端开发/在 Vite 项目中配置 @ 路径提示/","2025/03/16/前端开发/vue入门/","2025/03/13/hexo/如何用Hexo搭建个人博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
